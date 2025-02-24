532100f86dd67e01f2e74c0a175812a8eef45dbf
John Ryan Andal <john.andal@umtc.com.ph>
2025-02-24 10:49:13 +0800

    Added add user component

--- app/page.js
+++ app/page.js
@@ -2,6 +2,7 @@
 import Head from "next/head";
 import Navbar from "../components/Navbar";
 import UserList from "../components/UserList";
+import AddUser from "../components/AddUser";
 
 
 export default function Home() {
@@ -13,6 +14,7 @@ export default function Home() {
         </Head>
         <Navbar />
       <main>
+        <AddUser/>
         <UserList/>
       </main>
     </div>
---
+++ components/AddUser.js
@@ -0,0 +1,79 @@
+import { Dialog, Transition } from '@headlessui/react'
+import React, { Fragment, useState } from 'react'
+
+const AddUser = () => {
+    const [isOpen, setisOpen] = useState(false)
+
+    function closeModal() {
+        setisOpen(false)
+    }
+
+    function openModal() {
+        setisOpen(true)
+    }
+    
+  return (
+    <>
+        <div className='container mx-auto my-8'>
+            <div className='h-12'>
+                <button onClick={openModal} className='rounded bg-slate-600 text-white px-6 py-2 font-semibold'>
+                    Add User
+                </button>
+            </div>
+        </div>
+        <Transition appear show={isOpen} as={Fragment}>
+            <Dialog as="div" className="fixed inset-0 z-10 overflow-y-auto"
+            onClose={closeModal}>
+                <div className='min-h-screen px-4 text-center'>
+                    <Transition.Child
+                    as={Fragment}
+                    enter="ease-out duration-300"
+                    enterFrom='opacity-0 scale-95'
+                    enterTo='opacity-100 scale-100'
+                    leave='ease-in duration 200'
+                    leaveFrom='opacity-100 scale-100'
+                    leaveTo='opactiy-0 scale-95'>
+                        <div className='inline-block w-full max-w-md p-8 my-8 overflow-hidden text-left align-middle transition-all transform bg-white shadow-xl rounded-md'>
+                            <Dialog.Title as="h3" className="text-lg font-medium leading-6 text-gray-900">
+                                Add New User
+                            </Dialog.Title>
+                            <div className='flex max-w-md max-auto'>
+                                <div className='py-2'>
+                                    <div className='h-14 my-4'>
+                                        <label className='block text-gray-600 text-sm font-normal'>
+                                            First Name
+                                        </label>
+                                        <input type='text' name='firstName' className='h-10 w-96 border mt-2 py-2 px-2'></input>
+                                    </div>
+                                    <div className='h-14 my-4'>
+                                        <label className='block text-gray-600 text-sm font-normal'>
+                                            Last Name
+                                        </label>
+                                        <input type='text' name='firstName' className='h-10 w-96 border mt-2 py-2 px-2'></input>
+                                    </div>
+                                    <div className='h-14 my-4'>
+                                        <label className='block text-gray-600 text-sm font-normal'>
+                                            Email Id
+                                        </label>
+                                        <input type='text' name='firstName' className='h-10 w-96 border mt-2 py-2 px-2'></input>
+                                    </div>
+                                    <div className='h-14 my=-4 space-x-4 pt-4'>
+                                        <button className='rounded text-white font-semibold bg-green-400 hover:bg-green-700 py-2 px-6'>
+                                            Save
+                                        </button>
+                                        <button onClick={closeModal} className='rounded text-white font-semibold bg-red-400 hover:bg-red-700 py-2 px-6'>
+                                            Close
+                                        </button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </Transition.Child>
+                </div>
+            </Dialog>
+        </Transition>
+    </>
+  )
+}
+
+export default AddUser
